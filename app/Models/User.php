b4a70f324c7236609ca6de531dda151ed6cb8d1a
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-04-20 13:40:47 +0900

    add:postモデルとマイグレーションファイルを作成し、マイグレーションを実行する

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
